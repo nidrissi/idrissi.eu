b48a6c3ec8052be3a7bcebf670b73b77c9c29ede
Najib Idrissi <najib.idrissi.kaitouni@outlook.com>
2021-07-31 19:47:18 +0200

    switch to a DocumentClient

--- api/GetComments.cs
+++ api/GetComments.cs
@@ -1,9 +1,12 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Documents.Client;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using System.Collections.Generic;
+using System;
+using System.Linq;
 using System.Text.Json;
 
 namespace Idrissi.Blogging
@@ -14,12 +17,24 @@ namespace Idrissi.Blogging
         public static IActionResult Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "comment/{pageId}")]
             HttpRequest req,
+            string pageId,
             [CosmosDB(databaseName: "Blogging", collectionName: "Comments",
-                ConnectionStringSetting = "CosmosDbConnectionString",
-                SqlQuery = "select * from Blogging c where c.pageId = {pageId} order by c.date desc")]
-            IEnumerable<Comment> comments,
+                ConnectionStringSetting = "CosmosDbConnectionString")]
+            DocumentClient client,
             ILogger log)
         {
+            if (string.IsNullOrWhiteSpace(pageId))
+            {
+                return new NotFoundResult();
+            }
+            log.LogInformation($"Fetching comments for {pageId}");
+
+            Uri collectionUri = UriFactory.CreateDocumentCollectionUri("Blogging", "Comments");
+
+            var comments = from c in client.CreateDocumentQuery<Comment>(collectionUri)
+                           where c.pageId == pageId
+                           orderby c.date descending
+                           select c;
             var body = JsonSerializer.Serialize(comments);
             return new OkObjectResult(body);
         }
